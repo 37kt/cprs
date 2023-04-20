a83a26e0fbbf021628b6ca803847c9320d54d1bf
37kt <mata44junji@gmail.com>
2023-04-20 13:43:25 +0900

    a

---
+++ verify/examples/aplusb.rs
@@ -0,0 +1,11 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/aplusb
+
+use proconio::input;
+
+fn main() {
+    input! {
+        a: i64,
+        b: i64,
+    }
+    println!("{}", a + b);
+}
