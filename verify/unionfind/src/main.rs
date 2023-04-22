975e23f8f78674962c80655c61cbaecf91f1d1d4
37kt <mata44junji@gmail.com>
2023-04-23 00:43:19 +0900

    UnionFindのverifyを追加

---
+++ verify/unionfind/src/main.rs
@@ -0,0 +1,25 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
+
+use proconio::input;
+use union_find::UnionFind;
+
+#[proconio::fastout]
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+    }
+    let mut uf = UnionFind::new(n);
+    for _ in 0..q {
+        input! {
+            t: usize,
+            u: usize,
+            v: usize,
+        }
+        if t == 0 {
+            uf.merge(u, v);
+        } else {
+            println!("{}", uf.same(u, v) as i64);
+        }
+    }
+}
