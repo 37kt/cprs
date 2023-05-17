251a8bc9960f23bdf605245e1ea7bb43f395d8f3
37kt <mata44junji@gmail.com>
2023-05-17 16:30:46 +0900

    Rust 1.42.0で使えるように修正

--- crates/graph/graph/src/lib.rs
+++ crates/graph/graph/src/lib.rs
@@ -10,6 +10,18 @@ where
     edges: Vec<(usize, E)>,
 }
 
+pub const GRID_NEIGHBOURS_4: &[(usize, usize)] = &[(!0, 0), (0, !0), (1, 0), (0, 1)];
+pub const GRID_NEIGHBOURS_8: &[(usize, usize)] = &[
+    (!0, 0),
+    (0, !0),
+    (1, 0),
+    (0, 1),
+    (!0, !0),
+    (!0, 1),
+    (1, !0),
+    (1, 1),
+];
+
 impl<V, E> Graph<V, E>
 where
     V: Clone + Default,
@@ -53,6 +65,12 @@ where
         self.vertices[v] = w;
     }
 
+    pub fn add_vertex(&mut self, w: V) -> usize {
+        self.vertices.push(w);
+        self.head.push(!0);
+        self.vertices.len() - 1
+    }
+
     pub fn add_edge(&mut self, u: usize, v: usize, w: E) {
         self.next.push(self.head[u]);
         self.head[u] = self.edges.len();
@@ -71,11 +89,38 @@ where
     pub fn out_edges(&self, v: usize) -> impl Iterator<Item = &(usize, E)> {
         let mut e = self.head[v];
         std::iter::from_fn(move || {
-            (e != !0).then(|| {
+            if e != !0 {
                 let res = &self.edges[e];
                 e = self.next[e];
-                res
-            })
+                Some(res)
+            } else {
+                None
+            }
         })
+    }
+
+    pub fn from_grid(
+        grid: &Vec<Vec<V>>,
+        neighbours: &[(usize, usize)],
+        cost: impl Fn(&V, &V) -> Option<E>,
+    ) -> Self {
+        let h = grid.len();
+        let w = grid[0].len();
+        let mut g = Self::from(grid.into_iter().flatten().cloned().collect::<Vec<_>>());
+        for i in 0..h {
+            for j in 0..w {
+                for &(di, dj) in neighbours {
+                    let ni = i.wrapping_add(di);
+                    let nj = j.wrapping_add(dj);
+                    if ni >= h || nj >= w {
+                        continue;
+                    }
+                    if let Some(c) = cost(&grid[i][j], &grid[ni][nj]) {
+                        g.add_edge(i * w + j, ni * w + nj, c);
+                    }
+                }
+            }
+        }
+        g
     }
 }
