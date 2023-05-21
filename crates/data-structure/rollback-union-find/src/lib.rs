563a03efc07722274c2ec2bf68118b88dcb1bd59
37kt <mata44junji@gmail.com>
2023-05-21 09:38:35 +0900

    RollbackUnionFindを実装

---
+++ crates/data-structure/rollback-union-find/src/lib.rs
@@ -0,0 +1,66 @@
+use std::mem::swap;
+
+#[derive(Clone)]
+pub struct RollbackUnionFind {
+    par: Vec<i32>,
+    his: Vec<(usize, i32)>,
+}
+
+impl RollbackUnionFind {
+    pub fn new(n: usize) -> Self {
+        Self {
+            par: vec![-1; n],
+            his: vec![],
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.par.len()
+    }
+
+    pub fn merge(&mut self, x: usize, y: usize) -> bool {
+        let mut x = self.leader(x);
+        let mut y = self.leader(y);
+        self.his.push((x, self.par[x]));
+        self.his.push((y, self.par[y]));
+        if x == y {
+            return false;
+        }
+        if -self.par[x] < -self.par[y] {
+            swap(&mut x, &mut y);
+        }
+        self.par[x] += self.par[y];
+        self.par[y] = x as i32;
+        true
+    }
+
+    pub fn leader(&self, mut x: usize) -> usize {
+        while self.par[x] >= 0 {
+            x = self.par[x] as usize;
+        }
+        x
+    }
+
+    pub fn same(&self, x: usize, y: usize) -> bool {
+        self.leader(x) == self.leader(y)
+    }
+
+    pub fn size(&self, x: usize) -> usize {
+        -self.par[self.leader(x)] as usize
+    }
+
+    pub fn undo(&mut self) {
+        for _ in 0..2 {
+            let (x, par) = self.his.pop().unwrap();
+            self.par[x] = par;
+        }
+    }
+
+    pub fn groups(&self) -> Vec<Vec<usize>> {
+        let mut res = vec![vec![]; self.len()];
+        for x in 0..self.len() {
+            res[self.leader(x)].push(x);
+        }
+        res.into_iter().filter(|g| g.len() > 0).collect()
+    }
+}
