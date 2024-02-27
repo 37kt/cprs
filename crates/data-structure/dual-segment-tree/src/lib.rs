b307d73cbc609c0f9da40e1ee8f028bdfe35a0af
37kt <mata44junji@gmail.com>
2024-02-27 18:00:57 +0900

    非可換な演算に対応

--- crates/data-structure/dual-segment-tree/src/lib.rs
+++ crates/data-structure/dual-segment-tree/src/lib.rs
@@ -40,15 +40,15 @@ where
         l += self.n;
         r += self.n;
         self.propagate(l);
-        self.propagate(r - 1);
+        self.propagate(r);
         while l < r {
             if l & 1 != 0 {
-                self.v[l] = M::op(&self.v[l], &f);
+                self.v[l] = M::op(&f, &self.v[l]);
                 l += 1;
             }
             if r & 1 != 0 {
                 r -= 1;
-                self.v[r] = M::op(&self.v[r], &f);
+                self.v[r] = M::op(&f, &self.v[r]);
             }
             l >>= 1;
             r >>= 1;
@@ -66,17 +66,15 @@ where
         let mut res = self.v[k].clone();
         while k > 1 {
             k >>= 1;
-            res = M::op(&res, &self.v[k]);
+            res = M::op(&self.v[k], &res);
         }
         res
     }
 
     fn push(&mut self, i: usize) {
-        if i < self.n {
-            self.v[i * 2] = M::op(&self.v[i * 2], &self.v[i]);
-            self.v[i * 2 + 1] = M::op(&self.v[i * 2 + 1], &self.v[i]);
-            self.v[i] = M::e();
-        }
+        self.v[i * 2] = M::op(&self.v[i], &self.v[i * 2]);
+        self.v[i * 2 + 1] = M::op(&self.v[i], &self.v[i * 2 + 1]);
+        self.v[i] = M::e();
     }
 
     fn propagate(&mut self, i: usize) {
@@ -84,7 +82,7 @@ where
             return;
         }
         let crz = i.trailing_zeros() as usize;
-        for h in (crz + 1..63 - i.leading_zeros() as usize).rev() {
+        for h in (crz + 1..64 - i.leading_zeros() as usize).rev() {
             self.push(i >> h);
         }
     }
