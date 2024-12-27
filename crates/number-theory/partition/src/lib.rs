107f8b200f4bbba1b3de41e32fcdb3f008f8bcd6
37kt <117147526+37kt@users.noreply.github.com>
2024-12-27 10:55:45 +0900

    Delete crates directory

--- crates/number-theory/partition/src/lib.rs
+++
@@ -1,7 +0,0 @@
-use formal_power_series::FormalPowerSeries;
-use pentagonal_number_theorem::pentagonal_number_theorem;
-
-pub fn partition<const P: u32>(n: usize) -> FormalPowerSeries<P> {
-    let f = FormalPowerSeries(pentagonal_number_theorem(n));
-    f.inv(n)
-}
