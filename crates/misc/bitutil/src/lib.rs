cc4335807a77933513bb4509e1be530227bd4dd5
37kt <mata44junji@gmail.com>
2025-01-05 01:04:36 +0000

    bit列で表現した集合の部分集合の列挙を iter::successors を使ってきれいにした

--- crates/misc/bitutil/src/lib.rs
+++ crates/misc/bitutil/src/lib.rs
@@ -1,16 +1,11 @@
 /// ビットで表現された集合 s の部分集合を昇順に列挙
 pub fn bit_subsets(s: usize) -> impl Iterator<Item = usize> {
-    let mut x = 0;
-    std::iter::from_fn(move || {
-        let res = x;
-        if res == !0 {
+    std::iter::successors(Some(0usize), move |x| {
+        let y = x.wrapping_sub(s) & s;
+        if y == 0 {
             None
-        } else if x == s {
-            x = !0;
-            Some(res)
         } else {
-            x = x.wrapping_sub(s) & s;
-            Some(res)
+            Some(y)
         }
     })
 }
