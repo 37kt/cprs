f3fd4cfbfdc1780522bc566e4da27f367f571ce1
37kt <mata44junji@gmail.com>
2025-02-24 00:41:53 +0000

    古いファイルを退避

--- crates/number-theory_/partition/src/lib.rs
+++
@@ -1,9 +0,0 @@
-use formal_power_series::FormalPowerSeries;
-use pentagonal_number_theorem::pentagonal_number_theorem;
-
-/// 分割数 p(0), p(1), ... p(n) を列挙する  
-/// 分割数 p(n) は、 n を順番の違いを除いて自然数の和として表す方法の数
-pub fn partition<const P: u32>(n: usize) -> FormalPowerSeries<P> {
-    let f = FormalPowerSeries(pentagonal_number_theorem(n));
-    f.inv(n + 1)
-}
