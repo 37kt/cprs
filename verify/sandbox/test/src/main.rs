c155c71f7e0450324e83d2c917d9cdff341c25a5
37kt <mata44junji@gmail.com>
2025-04-04 09:50:17 +0000

    a

--- verify/sandbox/test/src/main.rs
+++ verify/sandbox/test/src/main.rs
@@ -1,10 +1,4 @@
 use proconio::fastout;
 
 #[fastout]
-fn main() {
-    let mut v = Vec::with_capacity(10);
-    for i in 0..100 {
-        v.resize(i, i);
-        eprintln!("{}", v.capacity());
-    }
-}
+fn main() {}
